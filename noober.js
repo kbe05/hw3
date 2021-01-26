0751f9fdda03f6c7c90b84fc1cf0462252c72bd8
Karim Hallab <karim.hallab@kellogg.northwestern.edu>
2021-01-25 20:57:01 -0600

    commit

--- noober.js
+++ noober.js
@@ -1,6 +1,66 @@
 function pageLoad() {
   // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
+  
+  let passenger1
+  let passenger2
+  let passenger3
 
+  levelOfService = 'Noober X'
+
+  passenger1 = ride[0]
+  passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
+  passenger1Phone = passenger1.passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = passenger1.numberOfPassengers
+  passenger1PickupAddressLine1 = passenger1.pickupLocation.address
+  passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
+  passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+  passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
+
+  if (passenger1.purpleRequested){
+    levelOfService = 'Noober Purple'
+  }
+  if (passenger1.numberOfPassengers > 3 ) {
+    levelOfService = 'Noober XL'
+  }
+
+  if (ride.length == 2) { 
+    passenger2 = ride[1]
+    passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
+    passenger2Phone = passenger2.passengerDetails.phoneNumber
+    passenger2NumberOfPassengers = passenger2.numberOfPassengers
+    passenger2PickupAddressLine1 = passenger2.pickupLocation.address
+    passenger2PickupAddressLine2 = passenger2.pickupLocation.city + ', ' + passenger2.pickupLocation.state + ' ' + passenger2.pickupLocation.zip
+    passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
+    passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
+    if (passenger1NumberOfPassengers == 1 && passenger2NumberOfPassengers ==1 ) {
+      levelOfService = 'Noober Pool'
+    }
+  } else if (ride.length > 2) {
+    passenger2 = ride[1]
+    passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
+    passenger2Phone = passenger2.passengerDetails.phoneNumber
+    passenger2NumberOfPassengers = passenger2.numberOfPassengers
+    passenger2PickupAddressLine1 = passenger2.pickupLocation.address
+    passenger2PickupAddressLine2 = passenger2.pickupLocation.city + ', ' + passenger2.pickupLocation.state + ' ' + passenger2.pickupLocation.zip
+    passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
+    passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
+    
+    passenger3 = ride[2]
+    passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
+    passenger3Phone = passenger3.passengerDetails.phoneNumber
+    passenger3NumberOfPassengers = passenger3.numberOfPassengers
+    passenger3PickupAddressLine1 = passenger3.pickupLocation.address
+    passenger3PickupAddressLine2 = passenger3.pickupLocation.city + ', ' + passenger3.pickupLocation.state + ' ' + passenger3.pickupLocation.zip
+    passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
+    passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + ', ' + passenger3.dropoffLocation.state + ' ' + passenger3.dropoffLocation.zip
+    if (passenger1NumberOfPassengers == 1 && passenger2NumberOfPassengers ==1 && passenger3NumberOfPassengers ==1 ) {
+      levelOfService = 'Noober Pool'
+    }
+  }
+
+  
+
+  
   // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
   // the 'ride' variable contains data about a random ride (i.e. when the page)
   // is refreshed or reloaded, the 'ride' variable will contain something different
